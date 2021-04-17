bef973a8c5abe30e4b8586de303a83f7cc0d8fb4
Anton Platonov <anton@platonov.us>
2021-04-16 22:37:25 -0400

    added test contract

---
+++ tests/contract_DnsRecordTEST.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# ==============================================================================
+#
+import freeton_utils
+from   freeton_utils import *
+
+class DnsRecordTEST(object):
+    def __init__(self, name: str, signer: Signer = None):
+        self.SIGNER      = generateSigner() if signer is None else signer
+        self.ABI         = "../bin/DnsRecordTEST.abi.json"
+        self.TVC         = "../bin/DnsRecordTEST.tvc"
+        self.CODE        = getCodeFromTvc(self.TVC)
+        self.CONSTRUCTOR = {}
+        self.INITDATA    = {"_domainName":stringToHex(name),"_domainCode": self.CODE}
+        self.PUBKEY      = ZERO_PUBKEY
+        self.ADDRESS     = getAddressZeroPubkey(abiPath=self.ABI, tvcPath=self.TVC, initialData=self.INITDATA)
+
+    def deploy(self, ownerID: str):
+        self.CONSTRUCTOR = {"ownerID": ownerID}
+        result = deployContract(abiPath=self.ABI, tvcPath=self.TVC, constructorInput=self.CONSTRUCTOR, initialData=self.INITDATA, signer=self.SIGNER, initialPubkey=self.PUBKEY)
+        return result
+
+    def call(self, functionName, functionParams, signer):
+        result = callFunction(abiPath=self.ABI, contractAddress=self.ADDRESS, functionName=functionName, functionParams=functionParams, signer=signer)
+        return result
+
+    def callFromMultisig(self, msig: Multisig, functionName, functionParams, value, flags):
+        messageBoc = prepareMessageBoc(abiPath=self.ABI, functionName=functionName, functionParams=functionParams)
+        result     = msig.callTransfer(addressDest=self.ADDRESS, value=value, payload=messageBoc, flags=flags)
+        return result
+
+    def run(self, functionName, functionParams):
+        result = runFunction(abiPath=self.ABI, contractAddress=self.ADDRESS, functionName=functionName, functionParams=functionParams)
+        return result
+
+    def destroy(self, addressDest):
+        result = self.call(functionName="TEST_selfdestruct", functionParams={"dest":freeton_utils.giverGetAddress()}, signer=self.SIGNER)
+        return result
+
+# ==============================================================================
+# 
